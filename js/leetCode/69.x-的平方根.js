c59b50fbaf107fc3b486d16feba3a1fba72867ef
yaoqifa <yaoqifa07330@hellobike.com>
2020-11-06 19:41:28 +0800

    69

---
+++ js/leetCode/69.x-的平方根.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=javascript
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  if (x === 0 || x === 1) {
+    return x
+  }
+  let l = 1;
+  let r = x;
+  let res
+  while (l <= r) {
+    let mid = parseInt((l + r) / 2)
+    if (mid * mid === x) {
+      return mid
+    } else if (mid * mid < x) {
+      l = mid + 1
+    } else {
+      r = mid - 1
+      res = r
+    }
+  }
+  return res;
+};
+// @lc code=end
+
