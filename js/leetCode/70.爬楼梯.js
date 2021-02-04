cc3e00ec44fa3c5030bc585d3c7fe8649d84c45c
yaoqifa <yaoqifa07330@hellobike.com>
2021-02-04 20:57:47 +0800

    dp 70

--- js/leetCode/70.爬楼梯.js
+++ js/leetCode/70.爬楼梯.js
@@ -13,13 +13,24 @@ var climbStairs = function(n) {
   if (n < 3) {
     return n
   }
-  let dp = Array.from({length: n})
-  dp[1] = 1
-  dp[2] = 2
+  let x = 1
+  let y = 2
   for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2]
+    [x, y] = [y, x + y]
   }
-  return dp[n]
+  return y
 };
+// var climbStairs = function(n) {
+//   if (n < 3) {
+//     return n
+//   }
+//   let dp = Array.from({length: n})
+//   dp[1] = 1
+//   dp[2] = 2
+//   for (let i = 3; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2]
+//   }
+//   return dp[n]
+// };
 // @lc code=end
 
