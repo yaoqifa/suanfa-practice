98afba87911d2e75a5b570150516f88ae7bf0308
yaoqifa <yaoqifa07330@hellobike.com>
2021-03-01 20:59:08 +0800

    72 dp

---
+++ js/leetCode/72.编辑距离.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  const m = word1.length
+  const n = word2.length
+  if (m === 0 && n === 0) {
+    return 0
+  }
+  const dp = Array.from({length: m + 1}, () => new Array(n + 1))
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i
+  }
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+      }
+    }
+  }
+  return dp[m][n]
+};
+// @lc code=end
+
