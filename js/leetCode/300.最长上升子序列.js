9064f70f334e27b54c2aa7109f21edfb56a69f67
yaoqifa <yaoqifa07330@hellobike.com>
2021-03-01 16:26:33 +0800

    300

---
+++ js/leetCode/300.最长上升子序列.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长上升子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  const len = nums.length
+  if (!len) {
+    return 0
+  }
+  let res = 1
+  const dp = new Array(len)
+  for (let i = 0; i < len; i++) {
+    dp[i] = 1
+  }
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+    res = Math.max(res, dp[i])
+  }
+  return res
+};
+// @lc code=end
+
