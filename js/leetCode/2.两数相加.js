9bdafba996ee1e9e7b745bb13ff25b8d8e414a6f
yaoqifa <yaoqifa07330@hellobike.com>
2021-04-15 20:59:29 +0800

    2

--- js/leetCode/2.两数相加.js
+++ js/leetCode/2.两数相加.js
@@ -18,7 +18,32 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+  let head = null
+  let tail = null
+  let carry = 0
+  while(l1 || l2) {
+    let n1 = l1 ? l1.val : 0
+    let n2 = l2 ? l2.val : 0
+    let res = n1 + n2 + carry
 
+    if (!head) {
+      head = tail = new ListNode(res % 10)
+    } else {
+      tail.next = new ListNode(res % 10)
+      tail = tail.next
+    }
+    carry = Math.floor(res / 10)
+    if (l1) {
+      l1 = l1.next
+    }
+    if (l2) {
+      l2 = l2.next
+    }
+  }
+  if (carry) {
+    tail.next = new ListNode(carry)
+  }
+  return head
 };
 // @lc code=end
 
